b045167d4a205a71c4c995e94f55d831d4cbb7e3
code <victorkbuzes@gmail.com>
2023-01-16 11:04:36 +0300

    mongo

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ app.use(express.urlencoded({ extended: false }));
 
 
 //mongodb
-mongoose.set('strictQuery', false);
-mongoose.connect(
-    process.env.DB_CONNECTION, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to Mongodb");
-    })
-    .catch((error) => {
-        console.log(error.message);
-        process.exit(1);
-    })
+// mongoose.set('strictQuery', false);
+// mongoose.connect(
+//     process.env.DB_CONNECTION, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => {
+//         console.log("Connected to Mongodb");
+//     })
+//     .catch((error) => {
+//         console.log(error.message);
+//         process.exit(1);
+//     })
 
 app.use(cors());
 
